67d1c6b0c9bc3072a8ae7f5c402e35ab856a571d
Kaushalya13 <nimanthikaushalya13@gmail.com>
2025-09-17 12:47:57 +0530

    add dependencies

---
+++ src/tailwind.config.ts
@@ -0,0 +1,41 @@
+// src/tailwind.config.ts
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#272727",
+        "light-gray": "#F5F5F5",
+        accent: "#A18A68",
+        "dark-text": "#333333",
+      },
+      fontFamily: {
+        satoshi: ["Satoshi", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      animation: {
+        'fade-in-up': 'fadeInUp 0.8s ease-in-out forwards',
+      },
+      keyframes: {
+        fadeInUp: {
+          '0%': { opacity: '0', transform: 'translateY(20px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
